6f60a4053121006b5b4a5fb5b4c9060fc8d4d6e7
Nick McGuffin <nicholas.mcguffin.developer@gmail.com>
2025-08-21 13:40:05 +0000

    Update linux_setup script

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt install python3 python3-build python3-pytest python3-pytest-cov setuptools setuptools-scm
+sudo apt install python3 python3-build python3-pytest python3-pytest-cov python3-setuptools python3-setuptools-scm
 
