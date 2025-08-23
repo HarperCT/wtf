c31abde5a388dbb5e812cb3be9ef60222ad7bf4b
Nick McGuffin <nicholas.mcguffin.developer@gmail.com>
2025-08-23 05:42:03 +0000

    Update TODO comment in plugin.py

--- python3_wtf/src/python3_wtf/plugins/plugin.py
+++ python3_wtf/src/python3_wtf/plugins/plugin.py
@@ -21,7 +21,7 @@ class Plugin(abc.ABC):
     @abc.abstractmethod
     def run(self) -> str:
         """
-            3 TODO not returning str... something else but can't
+            # TODO not returning str... something else but can't
             be None... i mean it could be i guess haven't thought
             what I want out of it yet. Expectation is that this will
             run the command and it will save to an output file
